f347dbbb48abf0396f651fb01abad6543b42207c
Emmanuel Otobo <eotobo11@gmail.com>
2023-03-19 20:04:00 -0230

    Fixed issue with comments not being persisted across routes; Added a little footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/shared/store';
+import { setPosts } from 'src/shared/store/post/post.actions';
+import { PostService } from './post/services/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private postService: PostService,
+    private store: Store<AppState>
+  ) {}
+  ngOnInit() {
+    this.postService
+      .getPosts()
+      .subscribe(posts => {
+        this.store.dispatch(setPosts({ posts }))
+      })
+  }
 }
--- src/app/post/components/postList/post-list.component.ts
+++ src/app/post/components/postList/post-list.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Store } from "@ngrx/store"
 import { AppState } from 'src/shared/store/';
-import { PostService } from '../../services/post.service';
-import * as POST_ACTIONS from 'src/shared/store/post/post.actions';
 import { IPost } from 'src/shared/store/post/post.model';
 
 @Component({
@@ -11,14 +9,13 @@ import { IPost } from 'src/shared/store/post/post.model';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent {
 
   posts: Observable<IPost[]>;
   filteredPosts: Observable<IPost[]>;
   searchQuery: string = ""
 
   constructor(
-    private postService: PostService,
     private store: Store<AppState>,
   ) {
     this.posts = this.store.select("posts")
@@ -29,14 +26,6 @@ export class PostListComponent implements OnInit {
     this.filteredPosts = this.posts.pipe(map(posts => {
       return posts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
     }))
-  }
-
-  ngOnInit() {
-    this.postService
-      .getPosts()
-      .subscribe(posts => {
-        this.store.dispatch(POST_ACTIONS.setPosts({ posts }))
-      })
   }
 }
 
